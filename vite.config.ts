c2d82c3d45fd95333bd5b3ca7e4bae4a7841b76f
Alex <alexx97354@gmail.com>
2025-03-03 12:34:00 +0100

    fix: changed vite config back to initial state

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // make the server accessible over the network
-    port: 8080
-  }
 })
-
